1f7ba893c1cce3260702b7949c8bf06fb6a054dc
Notjessand <jessennorom@gmail.com>
2024-05-27 00:59:36 -0500

    additional of carousels

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,10 @@ import Product from "@/components/home/product";
 import Innovations from "@/components/home/innovations";
 import Hero from "@/components/home/hero";
 import Experts from "@/components/home/experts";
-import { motion } from "framer-motion";
-import useOnScreen from "@/hooks/scroll";
+import Footer from "@/components/Footer";
+
 
 export default function Page() {
-  const [ref, isVisible] = useOnScreen({ threshold: 0.1 });
-
-  const variants = {
-    hidden: { opacity: 0, x: -350 },
-    visible: { opacity: 1, x: 0 },
-  };
-
   return (
     <div className=" my-0 py-0 font-custom overflow-hidden">
       <div className="min-h-screen bg-[url(https://res.cloudinary.com/dv62ty87r/image/upload/v1716664752/Mask-group-24_jnr5eg.jpg)] sm:bg-[url(https://res.cloudinary.com/dv62ty87r/image/upload/v1716660235/Mask-group-2_syfyo0.jpg)] bg-cover flex flex-col relative text-white bg-fixed bg-center">
@@ -24,8 +17,9 @@ export default function Page() {
         <Hero />
       </div>
       <Product />
-      <Innovations/>
-      <div className="h-screen"></div>
+      <Innovations />
+      <Experts />
+      <Footer/>
     </div>
   );
 }
--- components/home/experts.tsx
+++ components/home/experts.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import Carousel from "./carousel";
+import { motion } from "framer-motion";
+import useOnScreen from "@/hooks/scroll";
 
 const Experts = () => {
-  return (
-    <div>E</div>
-  )
-}
+  const [ref, isVisible] = useOnScreen({ threshold: 0.1 });
 
-export default Experts
+  const variants = {
+    hidden: { opacity: 0, x: 250 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  return (
+    <div className="pt-14 ">
+      <p className=" text-3xl font-montserrat text-center">
+        what experts are saying
+      </p>
+      <Carousel />
+
+      {/* experience the power */}
+      <div className=" w-11/12 sm:w-4/5 mx-auto bg-[url(https://res.cloudinary.com/dv62ty87r/image/upload/v1716788236/Mask-group-11_duexx1.jpg)] py-20 lg:py-0 lg:h-[90vh] rounded bg-cover bg-right mt-14 relative text-white">
+        {/* overlay */}
+        <div className="absolute h-full w-full inset-0 bg-gradient-to-b bg-black opacity-40 rounded"></div>
+        <div className="relative px-3 md:p-7 sm:p-14 font-montserrat text-center flex flex-col h-full justify-center">
+          <motion.div
+            // @ts-ignore
+            ref={ref}
+            initial="hidden"
+            animate={isVisible ? "visible" : "hidden"}
+            variants={variants}
+            transition={{ duration: 1 }}
+          >
+            <p className="text-5xl w-2/3 mx-auto font-montserrat font-semibold flex justify-center">
+              Experience the power of HIM wireless{" "}
+            </p>
+            <button className="py-3 px-16 mt-8 border border-blue-700 hover:border-white bg-blue-700 text-white text-lg rounded-full">Shop Now</button>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experts;
