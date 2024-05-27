cdb94667c334853f96220f77bbfe35481a7f83e5
Notjessand <jessennorom@gmail.com>
2024-05-27 02:05:21 -0500

    addition of footer modifications

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="h-screen px-5 sm:px-12 md:px-24 pt-20">
+    <div className="pb-7 px-5 sm:px-12 md:px-24 pt-20">
       <div className="w-full border-t border-slate-300 md:flex border-b pb-5">
         {/* newletter */}
         <div className="md:w-3/5 sm:min-h-[50vh] flex flex-col justify-center">
@@ -14,11 +14,10 @@ const Footer = () => {
               placeholder="email"
               className="py-2 rounded-full border-slate-600 w-80 sm:w-96 border px-2 mt-2 flex sm:inline mx-auto"
             />
-            <div className="flex sm:inline mx-auto justify-center text"> 
-
-            <button className="mt-3 flex justify-center sm:inline-block ml-1 py-2 px-12 sm:px-16 border border-blue-700 hover:border-orange-600 bg-blue-700 text-white text-lg rounded-full">
-              sign up
-            </button>
+            <div className="flex sm:inline mx-auto justify-center text">
+              <button className="mt-3 flex justify-center sm:inline-block ml-1 py-2 px-12 sm:px-16 border border-blue-700 hover:border-orange-600 bg-blue-700 text-white text-lg rounded-full">
+                sign up
+              </button>
             </div>
           </div>
           <p className="mt-3 text-sm sm:text-base">
@@ -44,6 +43,9 @@ const Footer = () => {
             </Link>
           </div>
         </div>
+      </div>
+      <div className="text-slate-500">
+        <p>© 2024 Thugger. All rights reserved</p>
       </div>
     </div>
   );
